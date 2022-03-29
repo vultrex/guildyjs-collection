daa74ad0ed8e8bd5f36a6f3ebe9f5ac3532322a7
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2022-03-29 19:32:19 -0400

    collection

---
+++ src/Collection.ts
@@ -0,0 +1,64 @@
+export class Collection<Key, Value> extends Map<Key, Value> {
+    public constructor(iterable?: Iterable<[Key, Value]>) {
+        super(iterable);
+    };
+
+    public get(key: Key): Value | undefined {
+        return super.get(key);
+    };
+
+    public has(key: Key): boolean {
+        return super.has(key);
+    };
+
+    public set(key: Key, value: Value): this {
+        super.set(key, value);
+        return this;
+    };
+
+    public delete(key: Key): boolean {
+        return super.delete(key);
+    };
+
+    public clear(): void {
+        super.clear();
+    };
+
+    public forEach(callbackfn: (value: Value, key: Key, map: this) => void, thisArg?: any): void {
+        super.forEach(callbackfn, thisArg);
+    };
+
+    public entries(): IterableIterator<[Key, Value]> {
+        return super.entries();
+    };
+
+    public keys(): IterableIterator<Key> {
+        return super.keys();
+    };
+
+    public restore(): void {
+        return super.clear();
+    };
+
+    public values(): IterableIterator<Value> {
+        return super.values();
+    };
+
+    public [Symbol.iterator](): IterableIterator<[Key, Value]> {
+        return super[Symbol.iterator]();
+    };
+
+    public [Symbol.toStringTag]: string = 'Collection';
+
+    public static from<Key, Value>(iterable: Iterable<[Key, Value]>): Collection<Key, Value> {
+        return new Collection(iterable);
+    };
+
+    public static fromArray<Key, Value>(array: [Key, Value][]): Collection<Key, Value> {
+        return new Collection(array);
+    };
+
+    public static fromPairs<Key, Value>(pairs: [Key, Value][]): Collection<Key, Value> {
+        return new Collection(pairs);
+    };
+}
